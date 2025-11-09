6e5d0e55e73e8edef73795be41192fad237c4b4b
Sean Collings <seanrcollings@gmail.com>
2025-11-09 13:57:52 -0700

    feat: configure custom domain ink.seancollings.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ink.seancollings.dev',
   vite: {
     plugins: [tailwindcss()]
   }
